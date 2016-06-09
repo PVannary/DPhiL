07441a2dd02c3c703da9ffbb0e0a69dec909e1fd
Vannary P Fowler <vannary.pov@gmail.com>
2016-06-08 23:21:45 -0400

    Adding application files

---
+++ application/core/Model.php
@@ -0,0 +1,42 @@
+<?php
+
+/* base model class */
+abstract class Model {
+    public $title;
+    public $description;
+
+    /**
+     * magic getter
+     */
+    public function __get($name) {
+        if ( isset($this->$name) ) {
+            return $this->$name;
+        }
+    }
+
+    /**
+     * magic setter
+     */
+    public function __set($name, $value) {
+        $this->$name = $value;
+    }
+
+    /**
+     * magic isset
+     */
+    public function __isset($name) {
+        return isset($this->$name);
+    }
+
+    /**
+     * magic destruct
+     */
+    public function __destruct() {}
+
+    /**
+     * magic unset
+     */
+    public function __unset($name) {
+        unset($this->$name);
+    }
+}
---
+++ application/models/HomeModel/HomeModel.php
@@ -0,0 +1,11 @@
+<?php
+
+/* home page displaying main content */
+class HomeModel extends Model{
+    const PAGE_TITLE = 'Home - GSU Delta Phi Lambda';
+
+    public function __construct() {
+        $this->title = self::PAGE_TITLE;
+    }
+
+}
