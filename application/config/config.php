96939e6c1b1a9f1e9a6782b997cf7becb0cf7165
Terrijon Fowler <tfowler4@student.gsu.edu>
2016-07-03 04:10:49 -0400

    added preamble/nationalhistory/chapter history to about page

--- application/config/config.php
+++ application/config/config.php
@@ -9,6 +9,7 @@ foreach ( glob(ABSOLUTE_PATH . '/application/lib/*.php') as $fileName ) { includ
 define('SITE_HEADER',    'Delta Phi Lambda Sorority, Inc.');
 define('SITE_SUBHEADER', serialize(array('Georgia State University', 'Gamma Chapter')));
 
+// links
 define('HOST_NAME', 'http://' . getHostByName(getHostName()) . '/site-dphil');
 
 define('SITE_NAV', serialize(
@@ -17,24 +18,24 @@ define('SITE_NAV', serialize(
                 'title' => 'About',
                 'url' => '',
                 'children' => array(
-                    array('title' => 'Chapter History',  'url' => 'about',           'children' => array()),
-                    array('title' => 'National History', 'url' => 'nationalhistory', 'children' => array()),
-                    array('title' => 'Preamble',         'url' => 'preamble',        'children' => array())
+                    array('title' => 'Chapter History',  'url' => 'about/chapter_history',  'children' => array()),
+                    array('title' => 'National History', 'url' => 'about/national_history', 'children' => array()),
+                    array('title' => 'Preamble',         'url' => 'about/preamble',         'children' => array())
                     )
                 ),
             'Meet the Sisters' => array(
                 'title' => 'Meet the Sisters',
                 'url' => '',
                 'children' => array(
-                    array('title' => 'Chapter Roster',  'url' => '', 'children' => array()),
-                    array('title' => 'Chapter Leaders', 'url' => '', 'children' => array()),
+                    array('title' => 'Chapter Roster',  'url' => 'sisters/roster', 'children' => array()),
+                    array('title' => 'Chapter Leaders', 'url' => 'sister/leaders', 'children' => array()),
                     )
                 ),
             'Recruitment' => array(
                 'title' => 'Recruitment',
                 'url' => '',
                 'children' => array(
-                    array('title' => 'Anti-Hazing', 'url' => 'antihazing', 'children' => array())
+                    array('title' => 'Anti-Hazing', 'url' => 'recruitment/antihazing', 'children' => array())
                     )
                 ),
             'Philanthropy' => array(
@@ -55,9 +56,6 @@ define('SITE_NAV', serialize(
         )
     )
 );
-// http://localhost/site-dphil/about/
-// links
-define('LINK_HOME', $_SERVER['DOCUMENT_ROOT'] . '/site-dphil');
 
 // images
 define('IMG_DFL_CREST', '/site-dphil/public/images/dfl_crest.png');
