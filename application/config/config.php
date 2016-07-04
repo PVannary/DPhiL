102f1c296f4b717984eb22a8d8dc703e775b8282
Terrijon Fowler <tfowler4@student.gsu.edu>
2016-07-03 23:17:15 -0400

    New footer and header adjustments

--- application/config/config.php
+++ application/config/config.php
@@ -7,7 +7,7 @@ foreach ( glob(ABSOLUTE_PATH . '/application/lib/*.php') as $fileName ) { includ
 
 // header
 define('SITE_HEADER',    'Delta Phi Lambda Sorority, Inc.');
-define('SITE_SUBHEADER', serialize(array('Gamma Chapter')));
+define('SITE_SUBHEADER', serialize(array('Georgia State University', 'Gamma Chapter')));
 
 // links
 define('HOST_NAME', 'http://' . getHostByName(getHostName()) . '/site-dphil');
@@ -17,40 +17,46 @@ define('SITE_NAV', serialize(
             'About' => array(
                 'title' => 'About',
                 'url' => '',
+                'icon' => 'fa fa-info',
                 'children' => array(
-                    array('title' => 'Chapter History',  'url' => 'about/chapter_history',  'children' => array()),
-                    array('title' => 'National History', 'url' => 'about/national_history', 'children' => array()),
-                    array('title' => 'Preamble',         'url' => 'about/preamble',         'children' => array())
+                    array('title' => 'Chapter History',  'url' => 'about/chapter_history',  'icon' => 'glyphicon glyphicon-asterisk', 'children' => array()),
+                    array('title' => 'National History', 'url' => 'about/national_history', 'icon' => 'glyphicon glyphicon-asterisk', 'children' => array()),
+                    array('title' => 'Preamble',         'url' => 'about/preamble',         'icon' => 'glyphicon glyphicon-asterisk', 'children' => array())
                     )
                 ),
-            'Meet the Sisters' => array(
-                'title' => 'Meet the Sisters',
+            'Sisters' => array(
+                'title' => 'Sisters',
                 'url' => '',
+                'icon' => 'fa fa-users',
                 'children' => array(
-                    array('title' => 'Chapter Roster',  'url' => 'sisters/roster', 'children' => array()),
-                    array('title' => 'Chapter Leaders', 'url' => 'sister/leaders', 'children' => array()),
+                    array('title' => 'Chapter Roster',  'url' => 'sisters/roster', 'icon' => 'glyphicon glyphicon-asterisk', 'children' => array()),
+                    array('title' => 'Chapter Leaders', 'url' => 'sister/leaders', 'icon' => 'glyphicon glyphicon-asterisk', 'children' => array()),
                     )
                 ),
             'Recruitment' => array(
                 'title' => 'Recruitment',
                 'url' => '',
+                'icon' => 'fa fa-file-text',
                 'children' => array(
-                    array('title' => 'Anti-Hazing', 'url' => 'recruitment/antihazing', 'children' => array())
+                    array('title' => 'Anti-Hazing', 'url' => 'recruitment/antihazing', 'icon' => 'glyphicon glyphicon-asterisk','children' => array())
                     )
                 ),
             'Philanthropy' => array(
                 'title'    => 'Philanthropy',
                 'url'      => 'philanthropy',
+                'icon'     => 'glyphicon glyphicon-tree-deciduous',
                 'children' => array()
                 ),
             'Gallery' => array(
                 'title'    => 'Gallery',
                 'url'      => 'gallery',
+                'icon'     => 'glyphicon glyphicon-picture',
                 'children' => array()
                 ),
             'Contact' => array(
                 'title'    => 'Contact',
                 'url'      => 'contact',
+                'icon'     => 'fa fa-envelope',
                 'children' => array()
                 )
         )
