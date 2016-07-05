d38bea0b3f5453e29a4f001d7c751e3277d97b17
Terrijon Fowler <tfowler4@student.gsu.edu>
2016-07-04 23:13:56 -0400

    Added database connection for documents

--- application/config/config.php
+++ application/config/config.php
@@ -83,3 +83,59 @@ define('SITE_NAV', serialize(
 // images
 define('IMG_DFL_CREST', HOST_NAME . '/public/images/dfl_crest.png');
 define('IMG_DFL_LOGO', HOST_NAME . '/public/images/dphil-logo.png');
+
+// DB Settings
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'xeonsold_dphil');
+define('DB_USER', 'xeonsold_dphil');
+define('DB_PASS', 'GSUdphil');
+
+/**
+ * database handling class
+ */
+class Db {
+    protected static $_dbh;
+
+    /**
+     * constructor
+     */
+    private function __construct() {
+        try {
+            self::$_dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=UTF8", DB_USER, DB_PASS);
+            self::$_dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch ( PDOException $e ) {
+            die('Connection error: ' . $e->getMessage());
+        }
+    }
+
+    /**
+     * initialize a database object if one doesn't already exist
+     *
+     * @return void
+     */
+    public static function init() {
+        if ( !self::$_dbh ) {
+            new Db();
+        }
+    }
+
+    /**
+     * get a database handler, create a new one if one doesn't exist
+     *
+     * @return self
+     */
+    public static function getDbh() {
+        if ( !self::$_dbh ) {
+            new Db();
+        }
+
+        return self::$_dbh;
+    }
+
+    /**
+     * magic clone
+     */
+    public function __clone() {}
+}
+
+Db::init();
