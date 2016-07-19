077ba31eb1eea7bfc1497f4ea6e80c24becc78a3
Terrijon Fowler <tfowler4@student.gsu.edu>
2016-07-18 23:15:36 -0400

    Added database table for roster and practice data

--- application/models/SistersModel/SistersModel.php
+++ application/models/SistersModel/SistersModel.php
@@ -5,6 +5,7 @@ class SistersModel extends Model {
     protected $_contentPage;
     protected $_pageContent;
     protected $_contentTitle;
+    protected $_rosterArray = array();
 
     const PAGE_TITLE = 'The Sisters - GSU Delta Phi Lambda';
 
@@ -28,11 +29,37 @@ class SistersModel extends Model {
             case 'roster':
                 $this->_contentTitle = 'Active Sisters';
                 $this->_pageContent = "";
+                $this->_rosterArray = $this->_getRosterFromDataBase();
                 break;
         }
     }
 
     protected function _getContentFromDatabase() {
         /* Database query here */
+    }
+
+    protected function _getRosterFromDatabase() {
+        /* Database query here */
+        $dbh         = DB::getDbh();
+        $rosterArray = array();
+
+        $query = $dbh->prepare(sprintf(
+            "SELECT roster_id,
+                    first_name,
+                    last_name,
+                    position,
+                    biography,
+                    cross_date,
+                    class,
+                    line_name
+               FROM roster_table"
+                ));
+        $query->execute();
+
+        while ( $row = $query->fetch(PDO::FETCH_ASSOC) ) {
+            $rosterArray[$row['roster_id']] = $row;
+        }
+
+        return $rosterArray;
     }
 }
