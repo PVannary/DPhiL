202b0786d3492030dd88bc956a881208c571d07e
Vannary P Fowler <vannary.pov@gmail.com>
2016-10-18 21:30:18 -0400

    Adding recruitment faq and antihazing statement page

--- application/models/RecruitmentModel/RecruitmentModel.php
+++ application/models/RecruitmentModel/RecruitmentModel.php
@@ -5,7 +5,6 @@ class RecruitmentModel extends Model {
     protected $_contentPage;
     protected $_pageContent;
     protected $_contentTitle;
-    protected $_menu     = array();
     protected $_faqArray = array();
 
     const PAGE_TITLE = 'Recruitment - GSU Delta Phi Lambda';
@@ -15,23 +14,9 @@ class RecruitmentModel extends Model {
             $this->_contentPage = $params[0];
             $this->_faqArray = $this->_getFaqFromDatabase();
             $this->_setContent();
-            $this->_setMenu();
         }
 
         $this->title = self::PAGE_TITLE;
-    }
-
-    protected function _setMenu() {
-        $this->_menu['header']  = 'FAQ';
-        $this->_menu['content'] = array();
-
-        foreach ( $this->_faqArray as $faqItem ) {
-            $listItem = array(
-                'title' => $faqItem['question'],
-                'attribute' => ''
-                );
-            $this->_menu['content'][] = $listItem;
-        }
     }
 
     protected function _setContent() {
@@ -40,6 +25,9 @@ class RecruitmentModel extends Model {
         switch($this->_contentPage) {
             case 'faqs':
                 $this->_contentTitle = 'Recruitment FAQs';
+                break;
+            case 'antihaze_statement':
+                $this->_contentTitle = 'Anti-Hazing Statement';
                 break;
         }
     }
