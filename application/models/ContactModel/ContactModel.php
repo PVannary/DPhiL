99e901199b9205a6f93cfb8dbcdc61e1ca60a360
Vannary Fowler <vannary.pov@gmail.com>
2016-10-20 18:44:40 -0400

    Adding function to check errors in contact page

--- application/models/ContactModel/ContactModel.php
+++ application/models/ContactModel/ContactModel.php
@@ -1,18 +1,39 @@
 <?php
 
 /* contact us page */
-class ContactModel extends Model{
+class ContactModel extends Model {
+    protected $_errorArray = array();
+
     const PAGE_TITLE = 'Contact Us - GSU Delta Phi Lambda';
 
     public function __construct() {
         $this->title = self::PAGE_TITLE;
 
-        //$this->_checkForErrors();
+        $this->_checkForErrors();
     }
 
     protected function _checkForErrors() {
         if ( !empty($_POST) ) {
             $errorsArray = array();
+
+            $name    = $_POST['inputName'];
+            $email   = $_POST['inputEmail'];
+            $subject = $_POST['inputSubject'];
+            $message = $_POST['inputMessage'];
+
+            if ( (empty($name)) || (empty($email)) || (empty($message)) ) {
+                $errorsArray[] = 'Please enter all required fields!';
+            } else {
+                if ( !filter_var($email, FILTER_VALIDATE_EMAIL) ) {
+                    $errorsArray[] = 'Please enter a valid email!';
+                }
+
+                if ( !ctype_alpha($name) ) {
+                    $errorsArray[] = 'Name may only contain alphabetic characters.';
+                }
+            }
+            //print_r($errorsArray);
+            return $errorsArray;
         }
     }
 }
