e3a1894804ca6e852cc6e540e091ba66c5e59e9b
Vannary P Fowler <vannary.pov@gmail.com>
2017-05-21 02:21:21 -0400

    Adding leaders files.

---
+++ application/controllers/leaders.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * leaders controller
+ */
+class Leaders extends AbstractController {
+    const CONTROLLER_NAME  = 'Leaders';
+    const PAGE_TITLE       = 'Leaders';
+    const PAGE_DESCRIPTION = 'Executive Board';
+
+    /**
+     * constructor
+     *
+     * @return void
+     */
+    public function __construct() {
+        parent::__construct();
+        $this->_setPageTitle();
+        $this->_setPageDescription();
+    }
+
+    /**
+     * index page of controller
+     *
+     * @param array [ url GET parameters ]
+     *
+     * @return void
+     */
+    public function index($params = array()) {
+        $this->_loadPageView('leaders/index', $this->_data);
+    }
+}
